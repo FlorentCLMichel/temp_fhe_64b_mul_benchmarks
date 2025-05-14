45fde9b1aca6ad47ea41abc2aebb0935784d95ea
Florent Michel <florent.michel@optalysys.com>
2025-05-14 13:34:07 +0100

    move the plaintext implementation

--- Makefile
+++ Makefile
@@ -1,14 +1,32 @@
-.PHONY: clean
+.PHONY: clean tests_plaintext
 
 CC := gcc
+PLAINTEXT_IMPL = plaintext_impl
 
-build/half_64b_mul_plaintext_1: src/include/mul_plaintext.h src/lib/mul_plaintext.c tests/half_64b_mul_plaintext_1.c
+build/half_64b_mul_plaintext_1:\
+	${PLAINTEXT_IMPL}/src/include/mul_plaintext.h\
+	${PLAINTEXT_IMPL}/src/lib/mul_plaintext.c\
+	${PLAINTEXT_IMPL}/tests/half_64b_mul_plaintext_1.c
 	mkdir -p build
-	$(CC) src/lib/mul_plaintext.c tests/half_64b_mul_plaintext_1.c -o $@ -Isrc/include
+	$(CC) ${PLAINTEXT_IMPL}/src/lib/mul_plaintext.c\
+	      ${PLAINTEXT_IMPL}/tests/half_64b_mul_plaintext_1.c\
+		  -o $@\
+		  -I${PLAINTEXT_IMPL}/src/include
 
-build/full_64b_mul_plaintext_1: src/include/mul_plaintext.h src/lib/mul_plaintext.c tests/full_64b_mul_plaintext_1.c
+build/full_64b_mul_plaintext_1:\
+	${PLAINTEXT_IMPL}/src/include/mul_plaintext.h\
+	${PLAINTEXT_IMPL}/src/lib/mul_plaintext.c\
+	${PLAINTEXT_IMPL}/tests/full_64b_mul_plaintext_1.c
 	mkdir -p build
-	$(CC) src/lib/mul_plaintext.c tests/full_64b_mul_plaintext_1.c -o $@ -Isrc/include
+	$(CC) ${PLAINTEXT_IMPL}/src/lib/mul_plaintext.c\
+	      ${PLAINTEXT_IMPL}/tests/full_64b_mul_plaintext_1.c\
+		  -o $@\
+		  -I${PLAINTEXT_IMPL}/src/include
+
+tests_plaintext: build/half_64b_mul_plaintext_1 build/full_64b_mul_plaintext_1
+	cp -ru ${PLAINTEXT_IMPL}/tests/data ./build/
+	cd build && ./half_64b_mul_plaintext_1
+	cd build && ./full_64b_mul_plaintext_1
 
 clean: 
 	rm -rf build
--- plaintext_impl/tests/full_64b_mul_plaintext_1.c
+++ plaintext_impl/tests/full_64b_mul_plaintext_1.c
@@ -4,16 +4,17 @@
 #include "mul_plaintext.h"
 
 #define N_INPUTS 1024
-#define FILENAME_LHS "tests/data/mul_data_1_in_lhs.dat"
-#define FILENAME_RHS "tests/data/mul_data_1_in_rhs.dat"
-#define FILENAME_OUT "tests/data/mul_data_1_out_half.dat"
+#define FILENAME_LHS "data/mul_data_1_in_lhs.dat"
+#define FILENAME_RHS "data/mul_data_1_in_rhs.dat"
+#define FILENAME_OUT "data/mul_data_1_out_full.dat"
 
 int main(int argc, char** argv) {
 
     FILE *file_lhs = NULL;
     FILE *file_rhs = NULL;
     FILE *file_out = NULL;
-    uint64_t lhs, rhs, correct_result;
+    uint64_t lhs, rhs;
+    uint64_t correct_result_high, correct_result_low;
     size_t read_count;
 
     // Open files for reading
@@ -60,8 +61,8 @@ int main(int argc, char** argv) {
                 return 1;
             }
 
-            read_count = fscanf(file_out, "%lu", &correct_result);
-            if (read_count != 1) {
+            read_count = fscanf(file_out, "%lu %lu", &correct_result_high, &correct_result_low);
+            if (read_count != 2) {
                 fprintf(stderr, "Error reading correct_result from file at index %u, %u\n", index_lhs, index_rhs);
                 fclose(file_lhs);
                 fclose(file_rhs);
@@ -69,7 +70,9 @@ int main(int argc, char** argv) {
                 return 1;
             }
             
-            assert(correct_result == half_64b_mul(lhs, rhs));
+            const uint128_t correct_result = (((uint128_t) correct_result_high) << 64) | correct_result_low;
+
+            assert(correct_result == full_64b_mul(lhs, rhs));
         }
        
         fclose(file_rhs);
