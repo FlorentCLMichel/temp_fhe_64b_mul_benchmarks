a69d67cb0065b55b2903381bcaf3d445e19f1702
Florent Michel <florent.michel@optalysys.com>
2025-05-20 13:52:28 +0100

    add structure for the full multiplier tests

--- implementation_0_tfhe_rs/src/he_mul.rs
+++ implementation_0_tfhe_rs/src/he_mul.rs
@@ -1,3 +1,4 @@
+use tfhe::prelude::*;
 use tfhe::FheUint64;
 
 pub fn half_cipher_cipher_mul_64(a: &FheUint64, b: &FheUint64) -> FheUint64 
@@ -8,4 +9,16 @@ pub fn half_cipher_cipher_mul_64(a: &FheUint64, b: &FheUint64) -> FheUint64
 pub fn half_cipher_plaintext_mul_64(a: &FheUint64, b: u64) -> FheUint64 
 {
     a * b
+}
+
+pub fn full_cipher_cipher_mul_64(a: &FheUint64, b: &FheUint64) -> (FheUint64, FheUint64)
+{
+    // TODO
+    unimplemented!()
+}
+
+pub fn full_cipher_plaintext_mul_64(a: &FheUint64, b: u64) -> (FheUint64, FheUint64)
+{
+    // TODO
+    unimplemented!()
 }
